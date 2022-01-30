2beff97518cec8dd3905ba417daba196bc049b62
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-30 13:21:28 -0300

    Puesta a punto del proyecto y se agrego libreria amotion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React from "react"
+import styled from "@emotion/styled"
 
+const Conteiner = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`
+
+const consultAPI = () => {
+  console.log('consultando...')
+}
 
 function App() {
   return (
-   <h1>Breaking Bad</h1>
+    <Conteiner>
+      <Button
+        onClick={() => consultAPI()}
+      >
+        Obtener Frase
+      </Button>
+    </Conteiner>
   )
 }
 
