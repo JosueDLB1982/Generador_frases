4d938aefb1184e30c6c3d257016be2f841524b12
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-30 16:46:18 -0300

    Aplicados estilos CSS a la frase respuesta de la API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import React from "react"
 import styled from "@emotion/styled"
 import Sentence from "./components/Sentence"
 
--- src/components/Sentence.js
+++ src/components/Sentence.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import styled from "@emotion/styled"
+
+const ConteinerSentence = styled.div`
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #FFF;
+    max-width: 800px;
+    margin-top: 15rem;
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`
 
 const Sentence = ({ sentence }) => {
     return (
-        <div>
+        <ConteinerSentence>
             <h1>
                 {sentence.quote}
             </h1>
             <p>
-                {sentence.author}
+                - {sentence.author}
             </p>
-        </div>
+        </ConteinerSentence>
     )
 }
 
